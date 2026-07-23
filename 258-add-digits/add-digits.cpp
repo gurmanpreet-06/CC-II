31b8e3f2348a1c0768a1dec4e9b02afd16432903
Gurmanpreet Singh <gps.us17@gmail.com>
2026-07-23 16:11:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (46.77%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -3,8 +3,10 @@ public:
     int addDigits(int num) {
         if (num == 0)
             return 0;
-        if (num % 9 == 0)
-            return 9;
-        return num % 9;
+        // if (num % 9 == 0)
+        //     return 9;
+        // return num % 9;
+
+        return num % 9 == 0 ? 9 : num % 9;
     }
 };
